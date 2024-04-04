3831b9d59f537487ac86e6716260b0fb8ac56e94
chizwheese <98509700+chizwheese@users.noreply.github.com>
2024-04-04 13:39:54 +0800

    Update app.js

--- app.js
+++ app.js
@@ -394,7 +394,7 @@ server.post('/edit-owner-profile', upload.single('pfpUrl'),  async (req, res) =>
 
 /* connect to database */
 
-mongoose.connect('mongodb+srv://chizwheese:maLaka$naPW!@cluster0.d6pztxh.mongodb.net/');
+mongoose.connect('mongodb+srv://chizwheese:maLaka$naPW!@cluster0.d6pztxh.mongodb.net/logininfo');
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Error connecting to database:'));
